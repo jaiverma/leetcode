f03a84c8e01d811d24c5b44bd6d8d9c586de70cf
Jai Verma <jai2.verma@outlook.com>
2019-12-23 17:44:57 +0530

    Adding better c++ solution to zigzag

--- zigzag/main.cpp
+++ zigzag/main.cpp
@@ -1,60 +1,46 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
+#include <numeric>
 
 class Solution {
-private:
-    int r;
-    int c;
-    int idx(int i, int j) {
-        return c * i + j;
-    }
-    int numCols(int n , int r) {
-        int c = 0;
-        while (n > 0) {
-            c++;
-            n -= r;
-            if (n > 0) {
-                c += r - 2;
-                n -= r - 2;
-            }
-        }
-        return c;
-    }
 public:
     std::string convert(std::string s, int numRows) {
-        if (s == "" || numRows < 2)
+        if (numRows == 1)
             return s;
-        int n = s.length();
-        this->r = numRows;
-        this->c = numCols(n, numRows);
-        std::string arr(this->c * this->r, ' ');
 
-        int i = 0, j = 0, x = 0;
-        while (j < this->c && x < n) {
-            if ((j % (this->r - 1)) == 0) {
-                i = 0;
-                while (i < r && x < n) {
-                    arr[idx(i, j)] = s[x];
-                    i++;
-                    x++;
-                }
-                i--;
-            }
-            else {
-                i--;
-                arr[idx(i, j)] = s[x];
-                x++;
-            }
-            j++;
+        std::vector<int> sz(numRows, 0);
+        int step = 1;
+        int rno = 0;
+        for (int i = 0; i < s.length(); i++) {
+            sz[rno]++;
+            rno += step;
+            if ((rno == 0) || (rno == numRows - 1))
+                step = -step;
         }
-        arr.erase(std::remove(arr.begin(), arr.end(), ' '), arr.end());
-        return arr;
+
+        step = 1;
+        rno = 0;
+        std::vector<int> cnos(numRows, 0);
+        std::string a(s.length(), 0);
+        for (int i = 0; i < s.length(); i++) {
+            if (rno == 0)
+                a[cnos[rno]] = s[i];
+            else
+                a[std::accumulate(sz.begin(), sz.begin() + rno, 0) + cnos[rno]] \
+                    = s[i];
+            cnos[rno] += 1;
+            rno += step;
+            if ((rno == 0) || (rno == numRows - 1))
+                step = -step;
+        }
+
+        return a;
     }
 };
 
 int main() {
     Solution s = Solution();
     std::string t = "PAYPALISHIRING";
-    int r = 3;
-    std::cout << s.convert(t, 3) << std::endl;
+    int r = 4;
+    std::cout << s.convert(t, r) << std::endl;
 }
